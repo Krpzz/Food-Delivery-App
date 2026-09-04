1d77991f105e09906f6c771b921601ee682a91d7
Krpzz <krpz54919@gmail.com>
2026-09-04 12:29:00 +0545

    bugs

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
-import restaurantReducer from './slices/restaurantSlice';
+import restaurantReducer from './slices/resturantSlice';
 import cartReducer from './slices/cartSlice';
 
 export const store = configureStore({
